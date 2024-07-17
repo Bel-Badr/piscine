74f82ecc51537a4d5ca8ea6c34c8d48be06bfa2d
BERLIN <158832806+Bel-Badr@users.noreply.github.com>
2024-07-17 15:47:54 +0100

    Add files via upload

---
+++ wdmatch.c
@@ -0,0 +1,40 @@
+#include <unistd.h>
+
+void    ft_putstr(char *str)
+{
+    while (*str)
+    {
+        write (1, str, 1);
+        str++;
+    }
+       
+}
+
+int main (int argc, char **argv)
+{
+    if (argc != 3)
+    {
+        write (1, "\n", 1);
+        return (0);
+    }
+    char *tmp;
+
+    if (argc == 3)
+    {
+        tmp = argv[1];
+        while (*tmp && *argv[2])
+        {
+            while (*tmp == *argv[2] && (*tmp && *argv[2]))
+            {
+                tmp++;
+                argv[2]++;
+            }
+            argv[2]++;
+        }
+        if (!(*tmp))
+        {
+            ft_putstr(argv[1]);
+        }
+    }
+    write (1, "\n", 1);
+}
